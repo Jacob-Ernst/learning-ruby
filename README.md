594d396d84700a37b1b19865a83573e3a02f840d
Jacob Ernst <akumadude.je@gmail.com>
2018-04-02 21:06:49 -0500

    Adding first exercise and more details for README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-## Learning Ruby
+# Learning Ruby
 Resources for beginners learning ruby. Just a WIP for now!
+
+## Exercises
+Under the `/exercises` directory there are challenges separated by folder.
+Each folder contains a series of tests which you can run as a guide to developing your own solution.
+
+You can clone this repository and add your own solutions to each exercise folder. Test your code by running the test suite alongside your solution. i.e. `ruby hello_world_test.rb`
+
+Comment out each successive `skip` to run the next test once your code passes the tests you've already run!
--- exercises/hello_world/hello_world_test.rb
+++ exercises/hello_world/hello_world_test.rb
@@ -1,5 +1,29 @@
-require 'minitest/autorun'
-require_relative 'hello_world'
+begin
+  gem 'minitest', '>= 5.0.0'
+  require 'minitest/autorun'
+  require 'minitest/pride'
+  require_relative 'hello_world'
+rescue Gem::LoadError => e
+  puts "\nMissing Dependency:\n#{e.backtrace.first} #{e.message}"
+  puts 'Minitest 5.0 gem must be installed for the Ruby track.'
+rescue LoadError => e
+  puts "\nError:\n#{e.backtrace.first} #{e.message}"
+  exit 1
+end
 
 class TestHelloWorld < Minitest::Test
+  def test_say_hello
+    assert_equal 'Hello World!', HelloWorld.hello
+  end
+
+  def test_say_hello_to_bill
+    skip
+    assert_equal 'Hello Bill!', HelloWorld.hello('Bill')
+  end
+
+  # Bonus Challenges!
+  def test_say_something_else
+    skip
+    assert_equal "Kon'nichiwa Bill!", HelloWorld.say_something(phrase: "Kon'nichiwa", friend: 'Bill')
+  end
 end
